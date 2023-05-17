515228f7b8801920bacfd8705aff26b394bac33c
Melanie Brown <brown.m@pm.me>
2023-05-17 15:34:12 -0400

    unused Response type

--- src/Network/SMTP/Response.hs
+++ src/Network/SMTP/Response.hs
@@ -1,29 +1,4 @@
-module Network.SMTP.Response (Response (..), ReplyCode (..)) where
+module Network.SMTP.Response (ReplyCode (..)) where
 
 newtype ReplyCode = ReplyCode Int
   deriving newtype (Eq, Ord, Show, Read, Enum, Num, Real, Integral)
-
-data Response
-  = OK
-  | SystemStatus
-  | HelpMessage
-  | ServiceReady
-  | ServiceClosing
-  | UserNotLocal
-  | CannotVerify
-  | StartMailInput
-  | ServiceNotAvailable
-  | MailboxUnavailable
-  | ErrorInProcessing
-  | InsufficientSystemStorage
-  | SyntaxError
-  | ParameterError
-  | CommandNotImplemented
-  | BadSequence
-  | ParameterNotImplemented
-  | MailboxUnavailableError
-  | UserNotLocalError
-  | ExceededStorage
-  | MailboxNotAllowed
-  | TransactionFailed
-  deriving (Eq, Show)
